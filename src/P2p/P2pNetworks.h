426e89cdd167f419fdd9a39ddf6c4435be762e0b
Codex02 <gamingcat11@gmail.com>
2018-02-06 20:03:46 -0500

    c
    c

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { { 0xAA ,0xA3, 0xA1, 0xC7 , 0xAA, 0xCA , 0xAC, 0xCC, 0xC4, 0x01, 0x00, 0x00, 0xC0, 0x11, 0xC1, 0x10} }; // Ivan's nightmare
+}
